0efb5432680f20e85f1898daed06286de07cd7aa
xiaowu <xiaohuiwu521@gmail.com>
2017-06-20 19:29:31 +0800

    delete an item from catr and item path bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -16,29 +16,8 @@ Rails.application.routes.draw do
     collection do
       delete :clean
     end
-
-  
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  resources :cart_items 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
